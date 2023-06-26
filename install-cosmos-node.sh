44c030277fbf05ebd0b829eeaa07d5afe36b1153
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-06-26 20:03:19 +0300

    Update install-cosmos-node.sh

--- install-cosmos-node.sh
+++ install-cosmos-node.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 clear
 # matrix (optional)
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/matrix.sh | bash
 # Logo
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/socrates.sh | bash
+# install binary function
+install_binary() {
+exec > /dev/null 2>&1
+git clone ...
+cd ...
+git checkout ...
+make install
+exec > /dev/tty 2>&1
+}
 # set variables
 BinaryName = "noded"
-nodename = "project"
+NodeName = "project"
 binaryversion = "v.1.1"
 ChainID = "chainid.l"
-customport = "111"
+CustomPort = "111"
+echo -e "\e[0;34m$NodeName Kurulumu Başlatılıyor\033[0m"
+sleep 3
 # get moniker
 echo -e '\e[0;35m' && read -p "Moniker isminizi girin: " MONIKER 
 echo -e "\033[035mMoniker isminiz\033[034m $MONIKER \033[035molarak kaydedildi"
@@ -15,72 +28,47 @@ echo -e '\e[0m'
 echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
 echo "export BinaryName=$BinaryName" >> $HOME/.bash_profile
 echo "export ChainID=$ChainID" >> $HOME/.bash_profile
-echo -e ''
-
-# remove old
-removeold() {
-sudo systemctl stop $BinaryName && \
-sudo systemctl disable $BinaryName && \
-rm /etc/systemd/system/$BinaryName.service && \
-sudo systemctl daemon-reload && \
-cd $HOME && \
-rm -rf .$BinaryName && \
-rm -rf $nodename && \
-rm -rf $(which $BinaryName)
-}
-removeold
-# prepare server
-# install go
-# install binary
-# init
-
-init() {
-$BinaryName config chain-id $ChainID
-$BinaryName config keyring-backend test
-$BinaryName config node tcp://localhost:${customport}57
-$BinaryName init $MONIKER --chain-id $ChainID
-}
-init
-
-# config
-
-sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 50/g' $HOME/.$BinaryName/config/config.toml
-sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 50/g' $HOME/.$BinaryName/config/config.toml
-sed -i \
-  -e 's|^pruning *=.*|pruning = "custom"|' \
-  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
-  -e 's|^pruning-keep-every *=.*|pruning-keep-every = "0"|' \
-  -e 's|^pruning-interval *=.*|pruning-interval = "19"|' \
-  $HOME/.$BinaryName/config/app.toml
-sleep 1
-sed -i -e 's|^indexer *=.*|indexer = "null"|' $HOME/.$BinaryName/config/config.toml
-
-sed -i -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${customport}58\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${customport}57\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${customport}60\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${customport}56\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${customport}66\"%" $HOME/.$BinaryName/config/config.toml
-sed -i -e "s%^address = \"tcp://localhost:1317\"%address = \"tcp://localhost:${customport}17\"%; s%^address = \":8080\"%address = \":${customport}80\"%; s%^address = \"localhost:9090\"%address = \"localhost:${customport}90\"%; s%^address = \"localhost:9091\"%address = \"localhost:${customport}91\"%; s%:8545%:${customport}45%; s%:8546%:${customport}46%; s%:6065%:${customport}65%" $HOME/.cascadiad/config/app.toml
-
-# service
-sudo systemctl stop $BinaryName
-sudo systemctl disable $BinaryName
-sudo rm -rf /etc/systemd/system/$BinaryName.service
-sudo tee /etc/systemd/system/$BinaryName.service > /dev/null <<EOF
-[Unit]
-Description=$nodename Node
-After=network-online.target
-[Service]
-User=$USER
-ExecStart=$(which $BinaryName) start --home $HOME/.$BinaryName
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
-[Install]
-WantedBy=multi-user.target
-EOF
-sleep 1
-# start
-sudo systemctl daemon-reload
-sudo systemctl enable $BinaryName
-sudo systemctl start $BinaryName
-sudo systemctl restart $BinaryName
+echo "export CustomPort=$CustomPort" >> $HOME/.bash_profile
+source $HOME/.bash_profile
 sleep 2
+echo -e ''
+# remove old
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/remove-cosmos-node.sh | bash
+echo -e "\e[0;34mSunucu Hazırlanıyor\033[0m"
+# prepare server
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/preparing-server.sh | bash
+echo -e "\e[0;34mGo Kuruluyor\033[0m"
+# install go
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/install-go.sh | bash
+echo -e "\e[0;33m$(go version) Kuruldu\033[0m"
+# install binary
+echo -e "\e[0;34m$BinaryName Kuruluyor\033[0m"
+install_binary
+echo -e "\e[0;33m$BinaryName $($BinaryName version) Kuruldu\033[0m"
+sleep 1
+echo -e "\e[0;34mYapılandırma Dosyası Ayarları Yapılıyor\033[0m"
+# init
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/init.sh | bash
+# config
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/config.sh | bash
+# service
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/systemctl.sh | bash
+# start
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/startnode.sh | bash
+sleep 1
 # print info
+echo -e "\e[0;34mNode Başlatıldı\033[0m"
+sleep 1
+echo -e ""
+echo -e "\e[0;32mLogları Görüntülemek İçin:\033[0;33m           sudo journalctl -u $BinaryName -fo cat\e[0m"
+echo -e ""
+echo -e ""
+sleep 1
+echo -e "\e[0;34mKurulum Tamamlandı\e[0m\u2600"
+sleep 1
 # Logo
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/socrates.sh | bash
+# yıldız
+curl -sSL https://raw.githubusercontent.com/0xSocrates/Scripts/main/y%C4%B1ld%C4%B1z.sh | bash
+
+
