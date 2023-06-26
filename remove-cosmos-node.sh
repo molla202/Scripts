cf8b7c8f0768bb358065a00966bab06fb40bdd65
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-06-26 18:10:40 +0300

    Update remove-cosmos-node.sh

--- remove-cosmos-node.sh
+++ remove-cosmos-node.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+removenode() {
+exec > /dev/null 2>&1
 sudo systemctl stop $BinaryName && \
 sudo systemctl disable $BinaryName && \
 rm /etc/systemd/system/$BinaryName.service && \
 sudo systemctl daemon-reload && \
 cd $HOME && \
 rm -rf .$BinaryName && \
-rm -rf $nodename && \
+rm -rf $NodeName && \
 rm -rf $(which $BinaryName)
+exec > /dev/tty 2>&1
+}
+removenode
