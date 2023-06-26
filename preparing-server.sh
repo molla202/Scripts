f7d0440e868e61673e9493ce148f6347aeef5eec
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-06-26 18:03:04 +0300

    Update preparing-server.sh

--- preparing-server.sh
+++ preparing-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+exec > /dev/null 2>&1
 sudo apt-get update -y && sudo apt-get upgrade -y
 sleep 1
 sudo apt install curl tar wget tmux htop net-tools clang pkg-config libssl-dev jq build-essential git screen make ncdu -y
+exec > /dev/tty 2>&1
