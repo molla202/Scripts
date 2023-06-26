df252ff9675279b1f4767b0b5a3afe9f18ea5b92
Socrates <108215275+0xSocrates@users.noreply.github.com>
2023-06-26 17:06:46 +0300

    Update install-cosmos-node.sh

--- install-cosmos-node.sh
+++ install-cosmos-node.sh
@@ -3,16 +3,18 @@ clear
 # matrix (optional)
 # Logo
 # set variables
-binary-name = noded
-node-name = project
-binary-version = v.1.1
-chainid = chainid.l
-custom-port = 111
+BinaryName = "noded"
+nodename = "project"
+binaryversion = "v.1.1"
+ChainID = "chainid.l"
+custom-port = "111"
 # get moniker
 echo -e '\e[0;35m' && read -p "Moniker isminizi girin: " MONIKER 
 echo -e "\033[035mMoniker isminiz\033[034m $MONIKER \033[035molarak kaydedildi"
 echo -e '\e[0m'
 echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
+echo "export BinaryName=$BinaryName" >> $HOME/.bash_profile
+echo "export ChainID=$ChainID" >> $HOME/.bash_profile
 echo -e ''
 
 # remove old
